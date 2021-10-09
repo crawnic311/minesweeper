f515115e8169039179a1b8f6485de95439e88845
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-09 13:43:31 -0600

    Created beginner window function

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+//Timer
 
+//Beginner Grid
+const beginnerGrid = document.querySelector('.beginner-grid')
+let width = 9
+let window = []
 
+//create window 
+function createWindow(event) {
+    for(let i = 0; i < width*width; i++) {
+        const square = document.createElement('div')
+        square.setAttribute('id', i)
+        beginnerGrid.appendChild(square)
+        window.push(square)
+    }
+}
 
+const beginnerButton = document.getElementById('#beginner-button')
+beginnerButton.addEventListener('click', testColor())
 
  
 
