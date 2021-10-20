eeda41418e210c17cb84122800d65d8fb4cc6fc6
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-20 15:47:49 -0600

    Grid populating

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-let beginnerBtn = document.getElementById('#beginner-button')
-console.log(beginnerBtn)
-beginnerBtn.addEventListener('click', changeColor)
 
-function changeColor(event) {
-    let intBtn = document.getElementById('#intermediate-button')
-    intBtn.style.backgroundColor = 'black'
+const beginnerBtn = document.getElementById('beginner-button')
+
+beginnerBtn.addEventListener('click', createGrid)
+
+
+const grid = document.getElementById('gridM')
+let width = 9
+let squares = []
+
+function createGrid() {
+    for(let i = 0; i < width * width; i++) {
+        const gridChild = document.createElement('div')
+        gridChild.setAttribute('id', i)
+        grid.appendChild(gridChild)
+        squares.push(gridChild)
+    }
 }
 
-changeColor()
+function changeColor() {
+    beginnerBtn.style.backgroundColor = "black"
+}
+
