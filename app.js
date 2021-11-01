f87f3447253c907e5a82f6da3bcad4a14f9caee9
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-11-01 11:13:13 -0600

    Bomb added

--- app.js
+++ app.js
@@ -7,17 +7,29 @@ beginnerBtn.addEventListener('click', createGrid)
 const grid = document.getElementById('gridM')
 let width = 9
 let squares = []
+let bombs = 20
 
 function createGrid() {
     for(let i = 0; i < width * width; i++) {
         const gridChild = document.createElement('div')
         gridChild.setAttribute('id', i)
+        gridChild.classList.add(shuffledArray[i])
         grid.appendChild(gridChild)
         squares.push(gridChild)
     }
+    bombPopulate()
 }
 
 function changeColor() {
     beginnerBtn.style.backgroundColor = "black"
 }
 
+
+function bombPopulate() {
+    const bombsArr = Array(bombs).fill('bomb')
+    const emptyArr = Array(width * width - bombs).fill('valid')
+    const gamesArr = emptyArr.concat(bombsArr)
+    const shuffledArr = gamesArr.sort(() => Math.random() - 0.5)
+    
+   
+}
